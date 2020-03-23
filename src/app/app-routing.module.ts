577ef7951d5135368e469a3ad3d99e8e0b4301db
ravinavadiya786 <ravinavadiya786@gmail.com>
2020-03-23 15:31:13 +0530

    remove extra files

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { FullLayoutComponent } from "./Main_layout/full-layout.component";
 
-import { ADMIN_ROUTES, FACULTY_ROUTES, STUDENT_ROUTES,Comman_ROUTES } from "./shared/routes/Routing_Config";
+import { ADMIN_ROUTES, FACULTY_ROUTES, STUDENT_ROUTES, Comman_ROUTES } from "./shared/routes/Routing_Config";
 
 import { AuthGuard } from './shared/auth/auth-guard.service';
 import { loginComponent } from './login/login.component';
@@ -22,7 +22,7 @@ const appRoutes: Routes = [
   { path: 'Admin', component: FullLayoutComponent, children: ADMIN_ROUTES, data: { roles: 'Admin' }, canActivate: [AuthGuard] },
   { path: 'Faculty', component: FullLayoutComponent, children: FACULTY_ROUTES, data: { roles: 'Faculty' }, canActivate: [AuthGuard] },
   { path: 'Student', component: FullLayoutComponent, children: STUDENT_ROUTES, data: { roles: 'Student' }, canActivate: [AuthGuard] },
-  { path: 'Comman', component: FullLayoutComponent, children: Comman_ROUTES,data: { roles: 'Comman' } },
+  { path: 'Comman', component: FullLayoutComponent, children: Comman_ROUTES, data: { roles: 'Comman' } },
 
 
 ];
